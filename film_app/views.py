884f6bc260a309eabcd0b28d42bbb68e096e623a
hackass <hackass@localhost.localdomain>
2019-01-25 13:59:06 -0800

    added first template - film_list.html

--- film_app/views.py
+++ film_app/views.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListViews
+from film_app.models import Film 
+
+class FilmList(ListViews):
+    model = Film
