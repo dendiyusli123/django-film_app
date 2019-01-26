c7f67be68917bea723e10dc5cbb05c3b8f4e9868
hackass <hackass@localhost.localdomain>
2019-01-26 10:31:51 -0800

    added 404 page

--- py_web/settings.py
+++ py_web/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '28nqd0h8tl-@#eq*)kzb67qnu@0y499l^cm!ezi)$l8i!z6dr@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
